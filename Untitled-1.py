8227db2694c60e2a6496e0badc27c3a00187ac3c
Christycx <christyxavier2005@gmail.com>
2025-10-30 23:12:32 +0530

    Delete Untitled-1.py

--- Untitled-1.py
+++
@@ -1,9 +0,0 @@
-@app.route('/add_expense', methods=['POST'])
-def add_expense():
-    data = request.get_json()
-    date = data.get('date')
-    expense_manager.add_expenses(data['category'],data['amount'], date)
-    return jsonify({'status': 'Expense added Successfully'})
-
-
-
